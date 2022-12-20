1120fa6a66d614b1a2f874a20429176ebdf5ba9e
Laura Maynards <121046553+LauraMaynards@users.noreply.github.com>
2022-12-20 14:52:31 +0100

    Update streamlit_app.py
    Changes ' place

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('My parents new healthy diner')
 
 st.header('Breakfast Favorites')
-st.text(:bowl_with_spoon:'Omega 3 & Blueberry Oatmeal')
-st.text(:green_salad: 'Kale, Spinach & Rocket Smoothie')
-st.text(:chicken: 'Hard-Boiled Free-Range Egg')
-st.text(:avocado: :bread: 'Avocado Toast')
+st.text(':bowl_with_spoon:Omega 3 & Blueberry Oatmeal')
+st.text(':green_salad: Kale, Spinach & Rocket Smoothie')
+st.text(':chicken: Hard-Boiled Free-Range Egg')
+st.text(':avocado: :bread: Avocado Toast')
