d12f357e127f1f86772d5168a97096d069782299
Laura Maynards <121046553+LauraMaynards@users.noreply.github.com>
2022-12-20 15:02:47 +0100

    Update streamlit_app.py
    Copy code from the course with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-
+  
 st.title('My parents new healthy diner')
 
 st.header('Breakfast Favorites')
-st.text(':bowl_with_spoon:Omega 3 & Blueberry Oatmeal')
-st.text(':green_salad: Kale, Spinach & Rocket Smoothie')
-st.text(':chicken: Hard-Boiled Free-Range Egg')
-st.text(':avocado: :bread: Avocado Toast')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
+st.text('Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
