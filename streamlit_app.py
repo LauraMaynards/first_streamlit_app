98bb66d3303c5060ebd071ab1eefd58d779544c3
Laura Maynards <121046553+LauraMaynards@users.noreply.github.com>
2022-12-20 14:51:00 +0100

    Update streamlit_app.py
    Changed header
    Added emojis
    Added new text line

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.title('My parents new healthy diner')
 
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Favorites')
+st.text(:bowl_with_spoon:'Omega 3 & Blueberry Oatmeal')
+st.text(:green_salad: 'Kale, Spinach & Rocket Smoothie')
+st.text(:chicken: 'Hard-Boiled Free-Range Egg')
+st.text(:avocado: :bread: 'Avocado Toast')
